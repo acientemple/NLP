4d73e4a841f39da7b5d62714e28d284b59731eae
Lenovo <acientemple@gmail.com>
2022-06-30 20:58:23 +0800

    Default Changelist

--- GuessLetter.py
+++ GuessLetter.py
@@ -17,13 +17,14 @@ print('该字母在%s这个词中！' % words_hint(number))
 while running==1:
    count+=1
    inword=input("请输入一个你猜的字母:")
-   if len(inword)==1 and inword.isaplpha():
+   if len(inword)==1 and inword.isalpha():
        words_hint(number)
        print('该字母也在%s这个词中！' % words_hint(number))
        if ord(inword) == number:
-           if count == 1:
+            if count == 1:
                print('祝贺你，第一次就猜对了！')
-           else:
+               running = 0
+            else:
                print("祝贺你, 你猜对了!")
                print("结束.")
                running = 0

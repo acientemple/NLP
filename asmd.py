f6476e7d48e6053bdafa6cd7663f95a6f3af6f06
Lenovo <acientemple@gmail.com>
2022-07-22 20:03:23 +0800

    Merge remote-tracking branch 'origin/master'

--- asmd.py
+++ asmd.py
@@ -7,7 +7,7 @@ f = open('calcu.txt', 'w')
 class Mix:
     """__m（默认50）题n（默认100）以内加减乘除"""
     import random
-    def __init__(self, m=50, n=100):
+    def __init__(self, m=60, n=100):
         self.m = m
         self.n = n
 
@@ -23,10 +23,10 @@ class Mix:
     # def set_n(self, score):
     #     self.n = score
 
-    # def __str__(self):
-    #     return f'{self.m}题{self.n}以内加减乘除'
-    #
-    # __repr__ = __str__
+    def __str__(self):
+        return f'{self.m}题{self.n}以内加减乘除'
+
+    __repr__ = __str__
 
     def all(self, sign=None):
         if sign is None:
@@ -36,17 +36,17 @@ class Mix:
             num1 = self.random.randint(1, self.n)
             num2 = self.random.randint(1, self.n)
             s = random.choice(sign)
-            # if (s == '÷' and num1 % num2 != 0):
-            #     continue
-            # elif (s == '-' and num1 < num2):
-            #     continue
-            # else:
-            count += 1
-            print('{0:<4d}{1:^3s}{2:<4d}  = '.format(num1, s, num2), end='\t\t')
-            print('{0:<4d}{1:^3s}{2:<4d}  = '.format(num1, s, num2), end='\t\t', file=f)
-            if (not count % 5):
-                print('\n')
-                print('\n', file=f)
+            if s == '÷' and num1 % num2 != 0:
+                continue
+            elif s == '-' and num1 < num2:
+                continue
+            else:
+                count += 1
+                print('{0:<4d}{1:^3s}{2:<4d}  = '.format(num1, s, num2), end='\t')
+                print('{0:<4d}{1:^3s}{2:<4d}  = '.format(num1, s, num2), end='\t', file=f)
+                if not count % 4:
+                    print('\n')
+                    print('\n', file=f)
 
     def add(self):
         self.all(sign=['+'])
--- asmd_print.py
+++ asmd_print.py
@@ -1,4 +1,12 @@
 import asmd
 all=asmd.Mix()
-all.add_sub()
+
+# all.add_sub()
+# all.add()
+# all.sub()
+
+all.n=10
+all.mul()
+# all.div()
+
 asmd.f.close()

8f35d3546faba8e51d25e26793654ee1269898b3
Lenovo <acientemple@gmail.com>
2022-10-26 19:03:44 +0800

    Merge remote-tracking branch 'origin/master'

--- nce.py
+++ nce.py
@@ -11,8 +11,9 @@ with open('nce.txt',encoding='utf8') as source:
     raw=source.read()
 
 tokens=nltk.word_tokenize(raw)
+sents=nltk.sent_tokenize(raw)
 tokens=[w for w in tokens if w.isalpha()]
-types=set(tokens)
+types=list(set(tokens)).sort()
 text=nltk.Text(tokens)
 fd=nltk.FreqDist(tokens)
 tokens_tagged=nltk.tag.pos_tag(tokens,lang='eng',tagset='universal')
